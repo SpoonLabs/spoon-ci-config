4dfc9ae2249079845b2125852c085e4381c659e2
Martin Monperrus <martin.monperrus@gnieh.org>
2019-09-20 21:51:51 +0200

    remove deleted jobs

--- jenkins/dl-jenkins.py
+++ jenkins/dl-jenkins.py
@@ -4,6 +4,9 @@
 import requests
 from lxml import etree
 import keyring
+import os
+import glob
+
 login_keyring=keyring.get_keyring() 
 
 
@@ -11,12 +14,18 @@ doc = requests.get("https://ci.inria.fr/sos/api/xml").text
 
 tree = etree.XML(doc)
 
+jobs = []
 for name in tree.xpath('//job/name/text()'):
   url = "https://ci.inria.fr/sos/job/"+name+"/config.xml"
   print(name)
   doc = requests.get(url, auth=requests.auth.HTTPBasicAuth('martin.monperrus@inria.fr', login_keyring.get_password('jenkins', 'martin.monperrus@inria.fr'))).text
+  jobs.append("jobs/"+name+".xml")
   with open("jobs/"+name+".xml","w") as e:
     e.write(doc)
+
+for i in glob.glob("jobs/*.xml"):
+    if i not in jobs:
+        os.system("git rm '"+i+"'")
 
 # getting the list of plugins
 plugins = requests.get("https://ci.inria.fr/sos/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins", auth=requests.auth.HTTPBasicAuth('martin.monperrus@inria.fr', login_keyring.get_password('jenkins', 'martin.monperrus@inria.fr'))).text
