f2598dbed0471a6293935eed2a96851c8a1e22d5
Martin Monperrus <martin.monperrus@gnieh.org>
2023-09-02 15:34:13 +0200

    @monperrus at work

--- jenkins/dl-jenkins.py
+++ jenkins/dl-jenkins.py
@@ -10,18 +10,20 @@ import glob
 login_keyring=keyring.get_keyring() 
 
 
-doc = requests.get("https://ci.inria.fr/sos/api/xml").text
+doc = requests.get("https://ci.inria.fr/sos/api/xml", auth=requests.auth.HTTPBasicAuth('martin.monperrus@inria.fr', login_keyring.get_password('jenkins', 'martin.monperrus@inria.fr'))).text
 
 tree = etree.XML(doc)
 
 jobs = []
 for name in tree.xpath('//job/name/text()'):
   url = "https://ci.inria.fr/sos/job/"+name+"/config.xml"
-  print(name)
+  print(url)
   doc = requests.get(url, auth=requests.auth.HTTPBasicAuth('martin.monperrus@inria.fr', login_keyring.get_password('jenkins', 'martin.monperrus@inria.fr'))).text
   jobs.append("jobs/"+name+".xml")
   with open("jobs/"+name+".xml","w") as e:
     e.write(doc)
+
+#print(jobs)
 
 for i in glob.glob("jobs/*.xml"):
     if i not in jobs:
